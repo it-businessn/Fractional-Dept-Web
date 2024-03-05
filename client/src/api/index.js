61ead80cf64bf75013cc0b87bdfa664c61f6948c
Juli Khosla <julikhosla17@gmail.com>
2024-03-05 14:24:45 -0800

    Minor fix to update correct api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL_LOCAL = "http://localhost:5000/api";
-const BASE_URL_LIVE = "https://fractional-dept-web.onrender.com";
+const BASE_URL_LIVE = "https://fractional-dept-web.onrender.com/api";
 
 export const API = axios.create({
 	baseURL: BASE_URL_LIVE,
